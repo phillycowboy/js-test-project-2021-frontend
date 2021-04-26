c32fb414aba0f33913c548929fa8253fd21a96f4
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-26 15:32:24 -0400

    tasker can now fetch users and tasks through an event listener each user append with their task and has a check box next to it for completion

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-alert('the pages are linked');
+// alert('the pages are linked');
 
-// next thing to do is to try and get the users and the tasks through a fetch request. 
+// you have both users and tasks through a fetch request.
+// Now what you need to focus on is how to get a form to submit and have the users tasks be pulled up 
+// how to add check box for task to be completed 
+// how to edit/delete a task 
+
 const baseUrl = "http://localhost:3000/users";
+const taskUrl = "http://localhost:3000/tasks";
+const tasksBtn = document.querySelector("#tasks-btn");
+const usersBtn = document.querySelector("#users-btn");
 
 function getUsers() {
      return fetch(baseUrl)
     .then(res => res.json())
     .then(data => renderUsers(data))
 };
-getUsers();
+// getUsers();
 
 function renderUsers(data){
     // console.log(data);
@@ -19,9 +26,37 @@ function renderUsers(data){
         let h1 = document.createElement('h1');
         h1.innerText = d.name;
         div.appendChild(h1);
-        console.log(d.name);
+        let h3 = document.createElement('h3');
+        h3.innerText = d.tasks[0].activity;
+        div.appendChild(h3);
+        let input = document.createElement('input');
+        input.type = "checkbox"
+        h3.appendChild(input);
         
     }
-}
+};
 
-// console.log("hello");
+function getTasks() {
+     return fetch(taskUrl)
+    .then(res => res.json())
+    .then(tasks => renderTasks(tasks))
+};
+// getTasks();
+
+function renderTasks(tasks) {
+    for(let t of tasks){
+        let div = document.createElement('div');
+        document.body.append(div);
+        let h1 = document.createElement('h1');
+        h1.innerText = t.activity;
+        div.appendChild(h1);
+        console.log(t.user_id);
+    }
+};
+
+tasksBtn.addEventListener('click', () => {
+    getTasks();
+})
+usersBtn.addEventListener('click', () => {
+    getUsers();
+})
