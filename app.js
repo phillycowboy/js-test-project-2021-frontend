b68f101fb94fe4ddbeeaa310939f9737eec24f9e
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-05-10 14:06:08 -0400

    starting to add from to home page

--- app.js
+++ app.js
@@ -11,64 +11,64 @@
 const baseUrl = "http://localhost:3000/users";
 const taskUrl = "http://localhost:3000/tasks";
 const homeUrl = "http://localhost:3000/"
-const tasksBtn = document.querySelector("#tasks-btn");
-const usersBtn = document.querySelector("#users-btn");
-const homeBtn = document.querySelector("#home-btn");
+// const tasksBtn = document.querySelector("#tasks-btn");
+// const usersBtn = document.querySelector("#users-btn");
+// const homeBtn = document.querySelector("#home-btn");
 
-function getUsers() {
-     return fetch(baseUrl)
-    .then(res => res.json())
-    .then(data => renderUsers(data))
-};
+// function getUsers() {
+//      return fetch(baseUrl)
+//     .then(res => res.json())
+//     .then(data => renderUsers(data))
+// };
 // getUsers();
 
-function renderUsers(data){
-    // console.log(data);
+// function renderUsers(data){
+//     // console.log(data);
     
-    for(let d of data){ 
-        let div = document.createElement('div');
-        document.body.append(div);
-        let h1 = document.createElement('h1');
-        h1.innerText = d.name;
-        div.appendChild(h1);
-        let h3 = document.createElement('h3');
-        h3.innerText = d.tasks[0].activity;
-        div.appendChild(h3);
-        let input = document.createElement('input');
-        input.type = "checkbox"
-        h3.appendChild(input);
+//     for(let d of data){ 
+//         let div = document.createElement('div');
+//         document.body.append(div);
+//         let h1 = document.createElement('h1');
+//         h1.innerText = d.name;
+//         div.appendChild(h1);
+//         let h3 = document.createElement('h3');
+//         h3.innerText = d.tasks[0].activity;
+//         div.appendChild(h3);
+//         let input = document.createElement('input');
+//         input.type = "checkbox"
+//         h3.appendChild(input);
         
-    }
-};
+//     }
+// };
 
-function getTasks() {
-     return fetch(taskUrl)
-    .then(res => res.json())
-    .then(tasks => renderTasks(tasks))
-};
+// function getTasks() {
+//      return fetch(taskUrl)
+//     .then(res => res.json())
+//     .then(tasks => renderTasks(tasks))
+// };
 // getTasks();
 
-function renderTasks(tasks) {
-    for(let t of tasks){
-        let div = document.createElement('div');
-        document.body.append(div);
-        let h1 = document.createElement('h1');
-        h1.innerText = t.activity;
-        div.appendChild(h1);
-        console.log(t.user_id);
-    }
-};
+// function renderTasks(tasks) {
+//     for(let t of tasks){
+//         let div = document.createElement('div');
+//         document.body.append(div);
+//         let h1 = document.createElement('h1');
+//         h1.innerText = t.activity;
+//         div.appendChild(h1);
+//         console.log(t.user_id);
+//     }
+// };
 
-tasksBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    getTasks();
-});
-usersBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    getUsers();
-});
-homeBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    window.location.reload();
-    // console.log(e)
-})
+// tasksBtn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     getTasks();
+// });
+// usersBtn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     getUsers();
+// });
+// homeBtn.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     window.location.reload();
+//     // console.log(e)
+// })
