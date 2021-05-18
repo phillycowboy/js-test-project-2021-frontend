eb5bfb3c7c1db463f43304899010f1b13560b7ab
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-05-17 20:10:55 -0400

    added new js file for refactoring

---
+++ src/app2.js
@@ -0,0 +1,8 @@
+// move queryselectors and eventlisteners in here to call the class functions and get them working 
+    // eventlistener calls function 
+        // function handles logic styling and call instance method
+            //instance method of class handles fetch  
+
+// make helper functions to hide or display divs and forms 
+
+// then worry aobut style/timeouts
